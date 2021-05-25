9570b1112105e61045d84ecbfcdd2bfda899eb2f
Alberto <info@peralstudio.com>
2021-05-25 07:40:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,14 +12,7 @@ import SignIn from './components/SignIn';
 import SignOut from './components/SignOut';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBMFVL_berT0wp1lFSguniGitDcQxBvsrg",
-  authDomain: "chatprueba-e0891.firebaseapp.com",
-  databaseURL: "https://chatprueba-e0891-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "chatprueba-e0891",
-  storageBucket: "chatprueba-e0891.appspot.com",
-  messagingSenderId: "324007932543",
-  appId: "1:324007932543:web:6699a30e22bca8e8eb054a",
-  measurementId: "G-R5KT4KNLTP"
+  ****************
 })
 
 const auth = firebase.auth();
