12289e1e4ff35521051090c6847e9b85fa2c097e
Alberto <info@peralstudio.com>
2021-05-27 11:35:05 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
-
 import { useAuthState } from 'react-firebase-hooks/auth';
 import ChatRoom from './components/ChatRoom';
+
 import SignIn from './components/SignIn';
-import SignOut from './components/SignOut';
 
 firebase.initializeApp({
   ****************
@@ -26,15 +25,9 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1>Sala: {roomName}</h1>
-        <SignOut />
-      </header>
-
       <section>
         {user ? <ChatRoom roomName={roomName} /> : <SignIn />}
       </section>
-
     </div>
   );
 }
